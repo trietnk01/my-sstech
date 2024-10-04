b600ddb68118633751d8278a7031723b32654f85
Nguyen Kim Dien <trietnk01@gmail.com>
2024-10-04 11:23:38 +0700

    Remove old version

--- stack-be/src/product/dto/create-product.input.ts
+++
@@ -1,9 +0,0 @@
-import { Field, InputType } from "@nestjs/graphql";
-import { MinLength } from "class-validator";
-
-@InputType()
-export class CreateProductInput {
-  @MinLength(1)
-  @Field((type) => String)
-  product_name: string;
-}
--- stack-be/src/product/dto/update-product.input.ts
+++
@@ -1,8 +0,0 @@
-import { Field, InputType, PartialType } from "@nestjs/graphql";
-import { CreateProductInput } from "./create-product.input";
-
-@InputType()
-export class UpdateProductInput extends PartialType(CreateProductInput) {
-  @Field(() => String)
-  _id: string;
-}
